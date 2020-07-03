9d82c70b3ee9e9314559336455c940dc845ab9ab
Ekrem TAŞ <ekremtas589@gmail.com>
2020-07-03 23:49:52 +0300

    Updates

---
+++ precache-manifest.47005051862bbaee03ade9abd59ed90b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd5153168bcb71e77151e1ef21ee65e",
+    "url": "/react-phone-comment-app/index.html"
+  },
+  {
+    "revision": "679529ed717d0a0d8f8a",
+    "url": "/react-phone-comment-app/static/css/2.92a0e85f.chunk.css"
+  },
+  {
+    "revision": "c23bae4b1f120e4e45a1",
+    "url": "/react-phone-comment-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "679529ed717d0a0d8f8a",
+    "url": "/react-phone-comment-app/static/js/2.099945d6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-phone-comment-app/static/js/2.099945d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23bae4b1f120e4e45a1",
+    "url": "/react-phone-comment-app/static/js/main.d0cf1e20.chunk.js"
+  },
+  {
+    "revision": "bba530ec02bfb3cf04af",
+    "url": "/react-phone-comment-app/static/js/runtime-main.db2cba24.js"
+  }
+]);
